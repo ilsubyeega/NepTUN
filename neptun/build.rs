02d420473ea6d8eae4b963cf278d309513dbf3af
ilsubyeega <ilsubyeega@outlook.com>
2026-01-21 19:31:26 +0900

    cocc
    fuck

--- neptun/build.rs
+++ neptun/build.rs
@@ -3,14 +3,7 @@ use std::process::Command;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Extract last commit sha using cli
-    let git_sha = String::from_utf8(
-        Command::new("git")
-            .args(["log", "-1", "--format=%h"])
-            .output()?
-            .stdout,
-    )?
-    .trim()
-    .to_string();
+    let git_sha = "unknown".to_string();
 
     // Output GIT_SHA as build time env variable
     println!("cargo:rustc-env=GIT_SHA={}", git_sha);
